99386e131146d4a866c63225e542a63de1d2ed13
derrk <47361858+derrk@users.noreply.github.com>
2022-05-10 22:41:21 -0500

    Manual upload to make this repo leave me alone

---
+++ myVectPractice1/myVectPractice1.cpp
@@ -0,0 +1,34 @@
+// Derrik Pollock - this program is for practice with using 
+// Vector member functions
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+	vector<int> values;
+
+	// Store Values in the vector
+	values.push_back(1);
+	values.push_back(2);
+	values.push_back(3);
+	cout << "The size of values is " << values.size() << endl;
+
+	// Remove a value from the vector
+	cout << "Popping a value from the vector...\n";
+	values.pop_back();
+	cout << "The size of values is now: " << values.size() << endl;
+
+	// Now remove another value from the function
+	cout << "Popping another value from the vector...\n";
+	values.pop_back();
+	cout << "The size of values is now: " << values.size() << endl;
+
+
+	// Remove the last value from the vector
+	cout << "Popping the last value from the vector...\n";
+	values.pop_back();
+	cout << "The size of values is now: " << values.size() << endl;
+	return 0;
+}
